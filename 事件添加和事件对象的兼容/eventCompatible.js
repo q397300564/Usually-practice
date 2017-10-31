77ea0e9698f3187c6718546d1c2f9628ae9b8b7f
罗丹 <luodan921221@gmail.com>
2017-10-31 15:49:30 +0800

    Add files via upload

---
+++ 事件添加和事件对象的兼容/eventCompatible.js
@@ -0,0 +1,61 @@
+var eventCompatible = {
+    // 添加事件处理程序的方法
+    getEvent: function(element, type ,handler){
+        if(!element){ return false };
+        if(element.addEventListener){
+            element.addEventListener( type, handler, false);
+            return true;
+        }
+        if(element.attachEvent){
+            element['e' + type + handler] = handler;
+            element[ type + handler] = function(){
+                element['e' + type + handler](window.event);
+            }
+
+            element.attachEvent( type , element[ type + handler]);
+            return true;
+        }
+        return false;
+    },
+
+    // 删除事件处理程序的方法
+    removeEvent: function(element, type, handler){
+        if(!element){ return false; }
+        if(element.removeEventListener){
+            element.removeEventListener( type, handler, false);
+            return true;
+        }
+        if(element.detachEvent){
+            element.detachEvent('on' + type + element[type + handler]);
+            element[type + handler] = null;
+        }
+    },
+
+    // 获取事件对象
+    getEvent: function(e){
+        return e || window.event;
+    },
+
+    // 获取目标事件
+    getTarget: function(e){
+        return e.target || e.scrElement;
+    }, 
+
+    // 取消默认事件
+    preventDefault: function(e){
+        if(e.preventDefault){
+            e.preventDefault();
+        }else{
+            e.returnValue = false;
+        }
+    },
+
+    // 取消事件冒泡
+    stopPropagation: function(e){
+        if(e.stopPropagation){
+            e.stopPropagation();
+        }else{
+            e.cancelBubble = true;
+        }
+    },
+};
---
+++ 事件添加和事件对象的兼容/readme.md
@@ -0,0 +1,28 @@
+创建了一个常用的工具对象，用于添加事件。尝试找出与IE的兼容问题。  只是练习
+
+这个是对象暂时的方法：
+
+添加事件
+addEvent(element, type, handler)
+
+删除事件
+removeEvent(element, type, handler)
+
+获取事件对象
+getEvent(e)
+
+获取目标事件
+getTarget(e)
+
+取消默认事件
+preventDefault(e)
+
+取消事件冒泡
+stopPropagation(e)
+
+获取事件的按钮
+getButton(e)
+
+获取事件滚动数据
+getWheelData(e)
+
